81891b2916a9f02925e71ec02a78f929a7936bc1
João da Mata <jlfilho@uea.edu.br>
2023-02-18 23:50:45 -0400

    endpoint categorias
    Criar categoria

--- src/main/java/uea/pagamentosapi/resources/CategoriaResource.java
+++ src/main/java/uea/pagamentosapi/resources/CategoriaResource.java
@@ -1,11 +1,17 @@
 package uea.pagamentosapi.resources;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Locale.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import uea.pagamentosapi.model.Categoria;
 import uea.pagamentosapi.services.CategoriaService;
@@ -18,8 +24,19 @@ public class CategoriaResource {
 	private CategoriaService categoriaService;
 	
 	@GetMapping
-	public List<Categoria> listar() {
-		return categoriaService.listar();
+	public ResponseEntity<List<Categoria>> listar() {
+		List<Categoria> categorias = categoriaService.listar();
+		return ResponseEntity.ok().body(categorias);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria) {
+		Categoria categoriaSalva = categoriaService.criar(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	
 
 }
--- src/main/java/uea/pagamentosapi/services/CategoriaService.java
+++ src/main/java/uea/pagamentosapi/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package uea.pagamentosapi.services;
 
 import java.util.List;
+import java.util.Locale.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,9 @@ public class CategoriaService {
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 	}
+	
+	public Categoria criar(Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	} 
 
 }
