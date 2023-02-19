e3518fcab8f9dced03d354bb90505c08970e2f72
João da Mata <jlfilho@uea.edu.br>
2023-02-18 21:33:40 -0400

    Criando o projeto da API

---
+++ src/test/java/uea/pagamentosapi/PagamentosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uea.pagamentosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
