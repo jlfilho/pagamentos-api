4b9a36765369720b85a5306ece0169c5dd94a5c7
João da Mata <jlfilho@uea.edu.br>
2023-02-20 02:29:53 -0400

    Endpoint pessoas: deletar

--- src/main/java/uea/pagamentosapi/services/PessoaService.java
+++ src/main/java/uea/pagamentosapi/services/PessoaService.java
@@ -5,6 +5,8 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import uea.pagamentosapi.model.Pessoa;
@@ -13,25 +15,29 @@ import uea.pagamentosapi.services.exceptions.ObjectNotFountException;
 
 @Service
 public class PessoaService {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
 	}
-	
+
 	public Pessoa criar(Pessoa pessoa) {
 		return pessoaRepository.save(pessoa);
-	} 
-	
+	}
+
 	public Pessoa buscarPorCodigo(Long codigo) {
 		try {
-			Optional<Pessoa> pessoa = pessoaRepository.findById(codigo); 
-			return pessoa.get();			
+			Optional<Pessoa> pessoa = pessoaRepository.findById(codigo);
+			return pessoa.get();
 		} catch (NoSuchElementException e) {
 			throw new ObjectNotFountException(codigo);
 		}
+	}
+
+	public void deletar(Long codigo) {
+		pessoaRepository.deleteById(codigo);
 	}
 
 }
