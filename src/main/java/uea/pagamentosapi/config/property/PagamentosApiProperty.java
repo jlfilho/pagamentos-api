99fc1791c008e697de2e759b63ac4a47b0891117
João da Mata <jlfilho@uea.edu.br>
2023-02-27 10:00:47 -0400

    Incluindo PagamentosApiProperty

---
+++ src/main/java/uea/pagamentosapi/config/property/PagamentosApiProperty.java
@@ -0,0 +1,50 @@
+package uea.pagamentosapi.config.property;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "pagamentosapi")
+public class PagamentosApiProperty {
+
+	private String originPermitida = "http://localhost:8000";
+
+	private final Seguranca seguranca = new Seguranca();
+
+	public Seguranca getSeguranca() {
+		return seguranca;
+	}
+
+	public String getOriginPermitida() {
+		return originPermitida;
+	}
+
+	public void setOriginPermitida(String originPermitida) {
+		this.originPermitida = originPermitida;
+	}
+
+	public static class Seguranca {
+
+		private List<String> redirectsPermitidos;
+		private String authServerUrl;
+
+		public List<String> getRedirectsPermitidos() {
+			return redirectsPermitidos;
+		}
+
+		public void setRedirectsPermitidos(List<String> redirectsPermitidos) {
+			this.redirectsPermitidos = redirectsPermitidos;
+		}
+
+		public String getAuthServerUrl() {
+			return authServerUrl;
+		}
+
+		public void setAuthServerUrl(String authServerUrl) {
+			this.authServerUrl = authServerUrl;
+		}
+	}
+
+}
