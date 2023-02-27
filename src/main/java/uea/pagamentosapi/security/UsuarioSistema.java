8d5db8359094a8564c2a6bfcf55d2bb582552ed7
João da Mata <jlfilho@uea.edu.br>
2023-02-27 09:58:16 -0400

    Incluindo UsuarioSistema e AppUserDetailsService

---
+++ src/main/java/uea/pagamentosapi/security/UsuarioSistema.java
@@ -0,0 +1,29 @@
+package uea.pagamentosapi.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import uea.pagamentosapi.models.Usuario;
+
+@JsonInclude(value=Include.NON_EMPTY)
+public class UsuarioSistema extends User {
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("usuario")
+	private Usuario usuario;
+
+	public UsuarioSistema(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
+		this.usuario = usuario;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+}
